3abf5ea9c6160ab47866e533e15e432df7afe16c
kateweb988 <katyavov2805@gmail.com>
2024-02-25 16:42:21 +0200

    fin

---
+++ src/js/main.js
@@ -0,0 +1,305 @@
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Scroll
+  $('.go_to').click(function () { // ловим клик по ссылке с классом go_to
+    var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
+    if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
+      $('html, body').animate({ scrollTop: $(scroll_el).offset().top - 50 }, 800); // анимируем скроолинг к элементу scroll_el
+    }
+    return false; // выключаем стандартное действие
+  });
+});
+document.addEventListener("DOMContentLoaded", () => {
+  //popup1
+  let popupBg = document.querySelector('.popup__bg');
+  let popup = document.querySelector('.popup');
+  let openPopupButtons = document.querySelectorAll('.nav__btn');
+  let closePopupButton = document.querySelector('.close-popup');
+
+  openPopupButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+      e.preventDefault();
+      popupBg.classList.add('active');
+      popup.classList.add('active');
+    })
+  });
+
+  closePopupButton.addEventListener('click', () => {
+    popupBg.classList.remove('active');
+    popup.classList.remove('active');
+  });
+
+  document.addEventListener('click', (e) => {
+    if (e.target === popupBg) {
+      popupBg.classList.remove('active');
+      popup.classList.remove('active');
+    }
+  });
+  document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape') {
+      //ваша функция закрытия окна
+      popupBg.classList.remove('active');
+      popup.classList.remove('active');
+    }
+  });
+});
+document.addEventListener("DOMContentLoaded", () => {
+  $('.menu li a').click(function (event) {
+    $('.menu-btn').toggleClass('active');
+    $('.menu').toggleClass('active');
+    return false;
+  });
+});
+window.addEventListener("DOMContentLoaded", function () {
+  [].forEach.call(document.querySelectorAll('.tel'), function (input) {
+    var keyCode;
+    function mask(event) {
+      event.keyCode && (keyCode = event.keyCode);
+      var pos = this.selectionStart;
+      if (pos < 3) event.preventDefault();
+      var matrix = "+7 (___) ___ ____",
+        i = 0,
+        def = matrix.replace(/\D/g, ""),
+        val = this.value.replace(/\D/g, ""),
+        new_value = matrix.replace(/[_\d]/g, function (a) {
+          return i < val.length ? val.charAt(i++) || def.charAt(i) : a
+        });
+      i = new_value.indexOf("_");
+      if (i != -1) {
+        i < 5 && (i = 3);
+        new_value = new_value.slice(0, i)
+      }
+      var reg = matrix.substr(0, this.value.length).replace(/_+/g,
+        function (a) {
+          return "\\d{1," + a.length + "}"
+        }).replace(/[+()]/g, "\\$&");
+      reg = new RegExp("^" + reg + "$");
+      if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
+      if (event.type == "blur" && this.value.length < 5) this.value = ""
+    }
+
+    input.addEventListener("input", mask, false);
+    input.addEventListener("focus", mask, false);
+    input.addEventListener("blur", mask, false);
+    input.addEventListener("keydown", mask, false)
+
+  });
+
+});
+document.addEventListener("DOMContentLoaded", () => {
+  (function ($) {
+    var elActive = '';
+    $.fn.selectCF = function (options) {
+
+      // option
+      var settings = $.extend({
+        color: "#888888", // color
+        backgroundColor: "#FFFFFF", // background
+        change: function () { }, // event change
+      }, options);
+
+      return this.each(function () {
+
+        var selectParent = $(this);
+        list = [],
+          html = '';
+
+        //parameter CSS
+        var width = $(selectParent).width();
+
+        $(selectParent).hide();
+        if ($(selectParent).children('option').length == 0) { return; }
+        $(selectParent).children('option').each(function () {
+          if ($(this).is(':selected')) { s = 1; title = $(this).text(); } else { s = 0; }
+          list.push({
+            value: $(this).attr('value'),
+            text: $(this).text(),
+            selected: s,
+          })
+        })
+
+        // style
+        var style = " background: " + settings.backgroundColor + "; color: " + settings.color + " ";
+
+        html += "<ul class='selectCF'>";
+        html += "<li>";
+        html += "<span class='arrowCF ion-chevron-right' style='" + style + "'></span>";
+        html += "<span class='titleCF' style='" + style + "; width:" + width + "px'>" + title + "</span>";
+        html += "<span class='searchCF' style='" + style + "; width:" + width + "px'><input style='color:" + settings.color + "' /></span>";
+        html += "<ul>";
+        $.each(list, function (k, v) {
+          s = (v.selected == 1) ? "selected" : "";
+          html += "<li value=" + v.value + " class='" + s + "'>" + v.text + "</li>";
+        })
+        html += "</ul>";
+        html += "</li>";
+        html += "</ul>";
+        $(selectParent).after(html);
+        var customSelect = $(this).next('ul.selectCF'); // add Html
+        var seachEl = $(this).next('ul.selectCF').children('li').children('.searchCF');
+        var seachElOption = $(this).next('ul.selectCF').children('li').children('ul').children('li');
+        var seachElInput = $(this).next('ul.selectCF').children('li').children('.searchCF').children('input');
+
+        // handle active select
+        $(customSelect).unbind('click').bind('click', function (e) {
+          e.stopPropagation();
+          if ($(this).hasClass('onCF')) {
+            elActive = '';
+            $(this).removeClass('onCF');
+            $(this).removeClass('searchActive'); $(seachElInput).val('');
+            $(seachElOption).show();
+          } else {
+            if (elActive != '') {
+              $(elActive).removeClass('onCF');
+              $(elActive).removeClass('searchActive'); $(seachElInput).val('');
+              $(seachElOption).show();
+            }
+            elActive = $(this);
+            $(this).addClass('onCF');
+            $(seachEl).children('input').focus();
+          }
+        })
+
+        // handle choose option
+        var optionSelect = $(customSelect).children('li').children('ul').children('li');
+        $(optionSelect).bind('click', function (e) {
+          var value = $(this).attr('value');
+          if ($(this).hasClass('selected')) {
+            //
+          } else {
+            $(optionSelect).removeClass('selected');
+            $(this).addClass('selected');
+            $(customSelect).children('li').children('.titleCF').html($(this).html());
+            $(selectParent).val(value);
+            settings.change.call(selectParent); // call event change
+          }
+        })
+
+        // handle search 
+        $(seachEl).children('input').bind('keyup', function (e) {
+          var value = $(this).val();
+          if (value) {
+            $(customSelect).addClass('searchActive');
+            $(seachElOption).each(function () {
+              if ($(this).text().search(new RegExp(value, "i")) < 0) {
+                // not item
+                $(this).fadeOut();
+              } else {
+                // have item
+                $(this).fadeIn();
+              }
+            })
+          } else {
+            $(customSelect).removeClass('searchActive');
+            $(seachElOption).fadeIn();
+          }
+        })
+
+      });
+    };
+    $(document).click(function () {
+      if (elActive != '') {
+        $(elActive).removeClass('onCF');
+        $(elActive).removeClass('searchActive');
+      }
+    })
+  }(jQuery));
+
+  $(function () {
+    var event_change = $('#event-change');
+    $(".select").selectCF({
+      change: function () {
+        var value = $(this).val();
+        var text = $(this).children('option:selected').html();
+        console.log(value + ' : ' + text);
+        event_change.html(value + ' : ' + text);
+      }
+    });
+  });
+});
+document.addEventListener('DOMContentLoaded', function () {
+  const swiper = new Swiper('.swiper1', {
+    slidesPerView: 1,
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next1',
+      prevEl: '.swiper-button-prev1',
+    },
+  });
+  const swiper2 = new Swiper('.swiper2', {
+    slidesPerView: 6,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: '.swiper-button-next2',
+      prevEl: '.swiper-button-prev2',
+    },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        spaceBetween: 0,
+        slidesPerView: 7
+      },
+      576: {
+        spaceBetween: 20,
+        slidesPerView: 3
+      },
+      767: {
+        spaceBetween: 20,
+        slidesPerView: 4
+      },
+      992: {
+        spaceBetween: 20,
+        slidesPerView: 4
+      },
+      1200: {
+        spaceBetween: 20,
+        slidesPerView: 6
+      }
+    }
+  });
+});
+document.addEventListener("DOMContentLoaded", () => {
+  let menuBtn = document.querySelector('.menu-btn');
+  let menu = document.querySelector('.menu');
+  menuBtn.addEventListener('click', function () {
+    menuBtn.classList.toggle('active');
+    menu.classList.toggle('active');
+  });
+});
+// svg
+$(function () {
+  jQuery('img.svg').each(function () {
+    var $img = jQuery(this);
+    var imgID = $img.attr('id');
+    var imgClass = $img.attr('class');
+    var imgURL = $img.attr('src');
+
+    jQuery.get(imgURL, function (data) {
+      // Get the SVG tag, ignore the rest
+      var $svg = jQuery(data).find('svg');
+
+      // Add replaced image's ID to the new SVG
+      if (typeof imgID !== 'undefined') {
+        $svg = $svg.attr('id', imgID);
+      }
+      // Add replaced image's classes to the new SVG
+      if (typeof imgClass !== 'undefined') {
+        $svg = $svg.attr('class', imgClass + ' replaced-svg');
+      }
+
+      // Remove any invalid XML tags as per http://validator.w3.org
+      $svg = $svg.removeAttr('xmlns:a');
+
+      // Check if the viewport is set, else we gonna set it if we can.
+      if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+        $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+      }
+
+      // Replace image with new SVG
+      $img.replaceWith($svg);
+
+    }, 'xml');
+
+  });
+});
